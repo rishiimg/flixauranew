87f98b1d7b8c5abd70fbe19c19ab7fc50f010ff8
shivtejimg <70063345+jitendrakumar0@users.noreply.github.com>
2024-10-01 18:34:47 +0530

    about us

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -32,94 +32,177 @@ About Us
 
 
 <section class="sm:py-20 py-16 relative overflow-hidden">
-            <div class="mx-auto max-w-7xl px-6 lg:px-8 w-full relative z-10">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8 w-full relative z-10">
 
-            <div class="grid grid-cols-12 items-center sm:gap-y-10 gap-y-5 lg:gap-8 group">
-                <div data-aos="fade-right" data-aos-duration="1000" class="col-span-12 lg:col-span-6 ">
-                <div class="size-full relative flex items-center justify-center group">
-                    <div class=" h-full w-[70%]">
-                    <Image width={500} height={500} class="aspect-[1/1]  object-cover rounded-3xl" src="{{asset('/public/tailwind_frontend/about-us.jpeg')}}" alt="" />
-                    </div>
+    <div class="grid grid-cols-12 items-center sm:gap-y-10 gap-y-5 lg:gap-8 group">
+      <div data-aos="fade-right" data-aos-duration="1000" class="col-span-12 lg:col-span-6 ">
+        <div class="size-full relative flex items-center justify-center group">
+          <div class=" h-full w-[70%]">
+            <Image width={500} height={500} class="aspect-[1/1]  object-cover rounded-3xl" src="{{asset('/public/tailwind_frontend/about-us.jpeg')}}" alt="" />
+          </div>
 
-                    <div class=" absolute sm:-top-7 -top-3  z-[3] w-[40%] right-0 sm:group-hover:right-[58%]  duration-300 ">
-                    <Image width={500} height={500} class="aspect-[1/.8] pointer-events-none rounded-3xl object-cover" src="{{asset('/public/tailwind_frontend/about-us2.jpeg')}}" alt="" />
-                    </div>
-                    <div class=" absolute -bottom-5 pointer-events-none z-[3] w-[40%] left-0 sm:group-hover:left-[58%]  duration-300 ">
-                    <Image width={500} height={500} class="aspect-[1/.8] rounded-3xl object-top" src="{{asset('/public/tailwind_frontend/about-us3.webp')}}" alt="" />
-                    </div>
-                </div>
-                </div>
-                <div data-aos="fade-left"  class="col-span-12 lg:col-span-6 ">
-                <div class="size-full">
-                    <div class="sm:text-5xl text-4xl relative  text-balance  w-full font-bold text-slate-800 lg:pb-14 max-lg:mt-20">
-                    Welcome to FLIXAURA Wedding Photography !
-                        <div class="absolute sm:-top-7 -top-3 [font-family:Yesteryear] [-webkit-text-stroke:_0.8px_#000] opacity-40 text-transparent md:text-[120px] sm:text-[90px] text-[80px] font-light">AboutUs</div>
-                    </div>
-                
-                    <div data-aos="zoom-out-up"  class="sm:text-base text-sm mt-4 text-slate-600 text-balance ">
-                    Welcome to FLIXAURA <strong>Wedding Photography</strong> , We specialize in capturing the magic, emotion, and unforgettable moments of your big day. With a passion for capturing genuine moments and heartfelt emotions, we create timeless images that reflect the unique beauty of each couple's love story. Our services offer high-quality <strong>wedding videography and  photography</strong> , with options for both candid and traditional styles. We also provide <strong>wedding videography packages</strong>  to suit your needs, whether it’s pre wedding shoot, cinematic video shoot, or full-day wedding coverage. Let us help you preserve cherished memories for generations to come.
+          <div class=" absolute sm:-top-7 -top-3  z-[3] w-[40%] right-0 sm:group-hover:right-[58%]  duration-300 ">
+            <Image width={500} height={500} class="aspect-[1/.8] pointer-events-none rounded-3xl object-cover" src="{{asset('/public/tailwind_frontend/about-us2.jpeg')}}" alt="" />
+          </div>
+          <div class=" absolute -bottom-5 pointer-events-none z-[3] w-[40%] left-0 sm:group-hover:left-[58%]  duration-300 ">
+            <Image width={500} height={500} class="aspect-[1/.8] rounded-3xl object-top" src="{{asset('/public/tailwind_frontend/about-us3.webp')}}" alt="" />
+          </div>
+        </div>
+      </div>
+      <div data-aos="fade-left" class="col-span-12 lg:col-span-6 ">
+        <div class="size-full">
+          <div class="sm:text-5xl text-4xl relative  text-balance  w-full font-bold text-slate-800 lg:pb-14 max-lg:mt-20">
+            Welcome to FLIXAURA Wedding Photography !
+            <div class="absolute sm:-top-7 -top-3 [font-family:Yesteryear] [-webkit-text-stroke:_0.8px_#000] opacity-40 text-transparent md:text-[120px] sm:text-[90px] text-[80px] font-light">AboutUs</div>
+          </div>
 
-                    </div>
-                </div>
-                </div>
+          <div data-aos="zoom-out-up" class="sm:text-base text-sm mt-4 text-slate-600 text-balance ">
+            Welcome to FLIXAURA <strong>Wedding Photography</strong> , We specialize in capturing the magic, emotion, and unforgettable moments of your big day. With a passion for capturing genuine moments and heartfelt emotions, we create timeless images that reflect the unique beauty of each couple's love story. Our services offer high-quality <strong>wedding videography and photography</strong> , with options for both candid and traditional styles. We also provide <strong>wedding videography packages</strong> to suit your needs, whether it’s pre wedding shoot, cinematic video shoot, or full-day wedding coverage. Let us help you preserve cherished memories for generations to come.
 
-            </div>
-            </div>
-        </section>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
 
 <!-- 2nd end  -->
- <!-- 3rd section -->
+<!-- 3rd section -->
 
- <div class="max-w-7xl mx-auto px-6 lg:px-8 py-10">
-            <div class="grid md:grid-cols-2  items-center md:gap-10 gap-5">
-                <div class="">
-                <div class="sm:text-5xl text-4xl relative  text-balance  w-full font-bold text-slate-800 md:pb-14 mb-8">
-                VISION
-                        <div class="absolute sm:-top-7 -top-3 [font-family:Yesteryear] [-webkit-text-stroke:_0.8px_#000] opacity-40 text-transparent md:text-[120px] sm:text-[90px] text-[80px] font-light">OUR</div>
-                    </div>
-                 
-                    <p
-                        class="sm:text-base text-sm mt-4 text-slate-600 text-balance text-ellipsis line-clamp4">
-                        A wedding is such a special and intimate celebration, turning fleeting moments into timeless memories.  We believe that every love story is unique, and our goal is to document yours in a way that reflects the emotions, joy, and beauty of the day.With a blend of artistic creativity and technical precision, we ensure that each moment is captured with care, leaving you with a collection of timeless memories to relive and share for generations to come. We aim to deliver <strong>wedding photographs</strong>  that you’ll treasure for a lifetime. Our vision is to provide you with an unforgettable experience, where every smile, every tear, and every cherished moment is preserved in stunning imagery that allows you to relive your special day forever.
-                    </p>
-                    <button class="flex justify-between text-slate-950 uppercase items-center overflow-hidden group/btn gap-4 z-20 relative mt-8 sn:text-base text-sm">
-                <span class='group-hover/btn:-translate-x-7 duration-300 w-7 h-[2px] bg-slate-950'></span> <span class='group-hover/btn:-translate-x-7 duration-300'> Explore
-                Wedding Films</span>
-              </button>
-                </div>
-                <div
-                    class="w-full  aspect-[1/.6] border-2 border-gray-200 shadow-2xl rounded-lg overflow overflow-hidden shadow-black/50 relative after:rotate-180 after:-z-10 after:absolute after:w-full after:h-1/2 after:shadow-xl after:bg-white after:inset-0 :size-full :object-cover object-top">
-                    <Image width={700} height={550}
-                        src="https://img.freepik.com/free-psd/3d-rendering-indian-wedding-characters_23-2151305901.jpg?t=st=1726136914~exp=1726140514~hmac=7d65788fff48a2af655ec6f62f05fed5df20c6449e72cfcc273c9c987f7abff0&w=740"
-                        alt="`" />
-                </div>
-            </div>
-            <div class="grid md:grid-cols-2  mt-20 items-center md:gap-10 gap-5">
-                <div class='md:order-2'>
-                <div class="sm:text-5xl text-4xl relative  text-balance  w-full font-bold text-slate-800 md:pb-14 mb-8">
-                MISSION
-                        <div class="absolute sm:-top-7 -top-3 [font-family:Yesteryear] [-webkit-text-stroke:_0.8px_#000] opacity-40 text-transparent md:text-[120px] sm:text-[90px] text-[80px] font-light">OUR</div>
-                    </div>
-                    <p
-                        class="sm:text-base text-sm mt-4 text-slate-600 text-balance text-ellipsis line-clamp4">
-         
-                    Our mission is to provide <strong>wedding photography</strong>  that captures the unique essence of each couple’s love story. We are dedicated to delivering stunning, high-quality images that reflect their unique personalities and the emotions of their special day. With a focus on creativity and storytelling, we strive to create a relaxed and enjoyable experience for our clients. We are dedicated to provide candid and <strong>cinematic photography</strong>  that you can cherish for generations, ensuring that every smile, tear, and joyous  moment is preserved in time.
-                    </p>
-                    <button class="flex justify-between text-slate-950 uppercase items-center overflow-hidden group/btn gap-4 z-20 relative mt-8 sm:text-base text-sm">
-                <span class='group-hover/btn:-translate-x-7 duration-300 w-7 h-[2px] bg-slate-950'></span> <span class='group-hover/btn:-translate-x-7 duration-300'> Explore
-                Wedding Films</span>
-              </button>
-                </div>
-                <div
-                    class="w-full aspect-[1/.6] md:order-1 border-2 border-gray-200 shadow-2xl rounded-lg overflow overflow-hidden shadow-black/50 relative after:rotate-180 after:-z-10 after:absolute after:w-full after:h-1/2 after:shadow-xl after:bg-white after:inset-0  :size-full :object-cover">
-                    <Image width={700} height={550}
-                        src="https://images.unsplash.com/flagged/photo-1572534779127-b64758946728?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+<div class="max-w-7xl mx-auto px-6 lg:px-8 py-10">
+  <div class="grid md:grid-cols-2  items-center md:gap-10 gap-5">
+    <div class="">
+      <div class="sm:text-5xl text-4xl relative  text-balance  w-full font-bold text-slate-800 md:pb-14 mb-8">
+        VISION
+        <div class="absolute sm:-top-7 -top-3 [font-family:Yesteryear] [-webkit-text-stroke:_0.8px_#000] opacity-40 text-transparent md:text-[120px] sm:text-[90px] text-[80px] font-light">OUR</div>
+      </div>
+
+      <p
+        class="sm:text-base text-sm mt-4 text-slate-600 text-balance text-ellipsis line-clamp4">
+        A wedding is such a special and intimate celebration, turning fleeting moments into timeless memories. We believe that every love story is unique, and our goal is to document yours in a way that reflects the emotions, joy, and beauty of the day.With a blend of artistic creativity and technical precision, we ensure that each moment is captured with care, leaving you with a collection of timeless memories to relive and share for generations to come. We aim to deliver <strong>wedding photographs</strong> that you’ll treasure for a lifetime. Our vision is to provide you with an unforgettable experience, where every smile, every tear, and every cherished moment is preserved in stunning imagery that allows you to relive your special day forever.
+      </p>
+      <button class="flex justify-between text-slate-950 uppercase items-center overflow-hidden group/btn gap-4 z-20 relative mt-8 sn:text-base text-sm">
+        <span class='group-hover/btn:-translate-x-7 duration-300 w-7 h-[2px] bg-slate-950'></span> <span class='group-hover/btn:-translate-x-7 duration-300'> Explore
+          Wedding Films</span>
+      </button>
+    </div>
+    <div
+      class="w-full  aspect-[1/.6] border-2 border-gray-200 shadow-2xl rounded-lg overflow overflow-hidden shadow-black/50 relative after:rotate-180 after:-z-10 after:absolute after:w-full after:h-1/2 after:shadow-xl after:bg-white after:inset-0 :size-full :object-cover object-top">
+      <img width={700} height={550}
+        src="https://image.wedmegood.com/resized/1900X/uploads/city_bg_image/1/delhi_bg.jpeg"
+        alt="`" />
+    </div>
+  </div>
+  <div class="grid md:grid-cols-2  mt-20 items-center md:gap-10 gap-5">
+    <div class='md:order-2'>
+      <div class="sm:text-5xl text-4xl relative  text-balance  w-full font-bold text-slate-800 md:pb-14 mb-8">
+        MISSION
+        <div class="absolute sm:-top-7 -top-3 [font-family:Yesteryear] [-webkit-text-stroke:_0.8px_#000] opacity-40 text-transparent md:text-[120px] sm:text-[90px] text-[80px] font-light">OUR</div>
+      </div>
+      <p
+        class="sm:text-base text-sm mt-4 text-slate-600 text-balance text-ellipsis line-clamp4">
+
+        Our mission is to provide <strong>wedding photography</strong> that captures the unique essence of each couple’s love story. We are dedicated to delivering stunning, high-quality images that reflect their unique personalities and the emotions of their special day. With a focus on creativity and storytelling, we strive to create a relaxed and enjoyable experience for our clients. We are dedicated to provide candid and <strong>cinematic photography</strong> that you can cherish for generations, ensuring that every smile, tear, and joyous moment is preserved in time.
+      </p>
+      <button class="flex justify-between text-slate-950 uppercase items-center overflow-hidden group/btn gap-4 z-20 relative mt-8 sm:text-base text-sm">
+        <span class='group-hover/btn:-translate-x-7 duration-300 w-7 h-[2px] bg-slate-950'></span> <span class='group-hover/btn:-translate-x-7 duration-300'> Explore
+          Wedding Films</span>
+      </button>
+    </div>
+    <div
+      class="w-full aspect-[1/.6] md:order-1 border-2 border-gray-200 shadow-2xl rounded-lg overflow overflow-hidden shadow-black/50 relative after:rotate-180 after:-z-10 after:absolute after:w-full after:h-1/2 after:shadow-xl after:bg-white after:inset-0  :size-full :object-cover">
+      <img class="size-full"
+        src="https://image.wedmegood.com/resized/1000X/uploads/member/967708/1614746342_BHU52052.jpg?crop=0,50,2048,1152
                 " alt="`" />
-                </div>
+    </div>
 
-            </div>
-        </div>
-  <!-- 3rd section end  -->
+  </div>
+</div>
+<!-- 3rd section end  -->
+
+
+<section class="h-full lg:py-20 py-12 bg-fixed bg-no-repeat bg-cover bg-center bg-[url('{{asset('/public/tailwind_frontend/contactform.webp')}}')]">
+	<div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl ">
+
+
+		<div class="max-w-5xl max-sm:px-4 mx-auto">
+
+			<div class="mt-6 overflow-hidden bg-white/20 shadow-xl rounded-xl">
+				<div class="p-6 sm:p-12">
+					<div class="text-white text-center text-[45px] sm:text-[70px] leading-[100%] font-normal [font-family:'Dancing_Script',cursive] pb-5">
+						Book Now
+					</div>
+					<div class=" text-sm md:text-base leading-7 text-white/80 text-balance text-center ">Planning your wedding or an event? Fill out the booking form, and we'll get in touch with you shortly.
+						If it's urgent, call us at +91-98870 34345.</div>
+
+					<form action="#" method="POST" class="sm:mt-14 mt-10">
+						<div class="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
+							<div>
+								<label for="" class="text-base font-medium text-white"> Your name </label>
+								<div class="mt-2.5 relative">
+									<input type="text" name="" id="" placeholder="Enter your full name" class="block w-full px-4 sm:py-4 py-2 text-black placeholder-gray-500 transition-all duration-200 bg-white/80 border border-gray-200 rounded-md focus:outline-none focus:border-slate-900 caret-slate-900" />
+								</div>
+							</div>
+
+							<div>
+								<label for="" class="text-base font-medium text-white"> Email address </label>
+								<div class="mt-2.5 relative">
+									<input type="email" name="" id="" placeholder="Enter your email" class="block w-full px-4 sm:py-4 py-2 text-black placeholder-gray-500 transition-all duration-200 bg-white/80 border border-gray-200 rounded-md focus:outline-none focus:border-slate-900 caret-slate-900" />
+								</div>
+							</div>
+
+							<div>
+								<label for="" class="text-base font-medium text-white"> Phone number </label>
+								<div class="mt-2.5 relative">
+									<input type="tel" name="" id="" placeholder="Phone Number" class="block w-full px-4 sm:py-4 py-2 text-black placeholder-gray-500 transition-all duration-200 bg-white/80 border border-gray-200 rounded-md focus:outline-none focus:border-slate-900 caret-slate-900" />
+								</div>
+							</div>
+
+							<div>
+								<label for="" class="text-base font-medium text-white"> City</label>
+								<div class="mt-2.5 relative">
+									<input type="text" name="" id="" placeholder="Type here..." class="block w-full px-4 sm:py-4 py-2 text-black placeholder-gray-500 transition-all duration-200 bg-white/80 border border-gray-200 rounded-md focus:outline-none focus:border-slate-900 caret-slate-900" />
+								</div>
+							</div>
+							<div>
+								<label for="" class="text-base font-medium text-white"> Service </label>
+								<div class="mt-2.5 relative">
+									<select id="countries" class="block w-full px-4 sm:py-4 py-2 text-black placeholder-gray-500 transition-all duration-200 bg-white/80 border border-gray-200 rounded-md focus:outline-none focus:border-slate-900 caret-slate-900">
+										<option selected>Select a Service</option>
+										<option value="Wedding">Wedding</option>
+										<option value="Pre-Wedding">Pre-Wedding</option>
+										<option value="Both">Both</option>
+									</select>
+								</div>
+							</div>
+							<div>
+								<label for="" class="text-base font-medium text-white"> Budget </label>
+								<div class="mt-2.5 relative">
+									<input type="text" name="" id="" placeholder="Type here..." class="block w-full px-4 sm:py-4 py-2 text-black placeholder-gray-500 transition-all duration-200 bg-white/80 border border-gray-200 rounded-md focus:outline-none focus:border-slate-900 caret-slate-900" />
+								</div>
+							</div>
+
+							<div class="sm:col-span-2">
+								<label for="" class="text-base font-medium text-white"> Message </label>
+								<div class="mt-2.5 relative">
+									<textarea name="" id="" placeholder="Type Here ...." class="block w-full px-4 sm:py-4 py-2 text-black placeholder-gray-500 transition-all duration-200 bg-white/80 border border-gray-200 rounded-md resize-y focus:outline-none focus:border-slate-900 caret-slate-900" rows="4"></textarea>
+								</div>
+							</div>
+
+							<div class="sm:col-span-2 text-center">
+								<button type="submit" class="inline-flex items-center justify-center w-auto px-4 py-2 mt-2 text-base font-semibold text-white transition-all duration-200 bg-slate-900 border border-transparent rounded-md focus:outline-none hover:bg-slate-800 focus:bg-slate-800">
+									Submit
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
 
 @endsection
 
